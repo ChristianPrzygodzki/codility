ad3fee1b6ef782c8151ce74f6f3581b97d3d3dd7
ChristianPrzygodzki <christian.przygodzki@gmail.com>
2020-10-27 00:57:42 +0100

    Create Codility repository
    Add full first task

---
+++ src/lesson1/Solution.java
@@ -0,0 +1,26 @@
+package lesson1;
+
+// Binary gap task
+// Description:
+// https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
+
+class Solution {
+    public int solution(int N) {
+        String binaryRep = Integer.toBinaryString(N);
+        int maxGap = 0;
+        String[] words = binaryRep.split("1");
+        for(int i=0; i < words.length-1; i++){
+//          System.out.println(words[i]);
+            if(words[i].length()>maxGap ){
+                maxGap = words[i].length();
+            }
+        }
+        if(binaryRep.charAt(binaryRep.length()-1)=='1'){
+            if(words.length > 0 && words[words.length-1].length()>maxGap ){
+                maxGap = words[words.length-1].length();
+            }
+        }
+//      System.out.println("lenght: " + words.length);
+        return maxGap;
+    }
+}
