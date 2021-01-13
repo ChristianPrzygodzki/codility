fee73fe505e4a70cbb689e16c21fe3dc24ac252b
ChristianPrzygodzki <christian.przygodzki@gmail.com>
2021-01-13 23:44:45 +0100

    Add Class file for Stone Wall task
    I have got some idea to try, but have not started to code.

---
+++ src/lesson7/StoneWall.java
@@ -0,0 +1,16 @@
+package lesson7;
+
+// Stone Wall task
+// Description:
+// https://app.codility.com/programmers/lessons/7-stacks_and_queues/stone_wall/
+// Score: , Correctness:
+
+public class StoneWall {
+    public int solution(int[] H) {
+        // I want to try an idea to divide all wall to rectangles
+        // as long as possible horizontally, then give them extra
+        // height if possible. However I do not know yet if it is
+        // always optimal (lowest nr).
+        return 0;
+    }
+}
