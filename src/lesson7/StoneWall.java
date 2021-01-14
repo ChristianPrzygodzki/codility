f3e20d6a2280c8bc332455fd94f70707c741b0cb
ChristianPrzygodzki <christian.przygodzki@gmail.com>
2021-01-15 00:53:22 +0100

    Start Stone Wall task
    Completed easy part, the construction of list of small walls will be the hard part.

--- src/lesson7/StoneWall.java
+++ src/lesson7/StoneWall.java
@@ -5,12 +5,42 @@ package lesson7;
 // https://app.codility.com/programmers/lessons/7-stacks_and_queues/stone_wall/
 // Score: , Correctness:
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StoneWall {
     public int solution(int[] H) {
         // I want to try an idea to divide all wall to rectangles
         // as long as possible horizontally, then give them extra
         // height if possible. However I do not know yet if it is
         // always optimal (lowest nr).
+        int counter = 0;
+
         return 0;
+    }
+    private int giveMinimalNumberOfRectangles(int[] wall){
+
+        int minimalHeight = getMinimalHeight(wall);
+
+        for (int i = 0; i < wall.length; i++) {
+            wall[i]-=minimalHeight;
+        }
+        List<int[]> smallWalls = new ArrayList<int[]>();
+        // TODO smallWalls construction
+        int result = 1; // long rectangle at the bottom
+        for(int[] smallWall:smallWalls){
+            result+=giveMinimalNumberOfRectangles(smallWall);
+        }
+        return result;
+    }
+
+    private int getMinimalHeight(int[] wall){
+        int min = Integer.MAX_VALUE;
+        for(int partHeight:wall){
+            if(partHeight<min){
+                min = partHeight;
+            }
+        }
+        return min;
     }
 }
